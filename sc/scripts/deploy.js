0423947421fbbd90cddf51aaae0f34ac2b15afa5
Robert Spadinger <rspadinger@gmail.com>
2026-04-04 10:57:54 +0200

    update deploy and interact files

--- sc/scripts/deploy.js
+++ sc/scripts/deploy.js
@@ -1,18 +1,13 @@
-const { VRF_COORDINATOR, KEY_HASH, SUBSCRIPTION_ID, ETH_USD_FEED, LOOT_USD_FEED, USDC } = process.env
-
 async function main() {
-    // Deploy Mock contracts
-    const MockERC20 = await ethers.getContractFactory("MockERC20")
-    const mockUsdcToken = await MockERC20.deploy("USDC", "USDC", 6, ethers.parseUnits("1000000", 6))
+    const myToken = await ethers.deployContract("StakingToken")
+    await myToken.waitForDeployment()
 
-    // Deploy NAME
-    const Contract1 = await ethers.getContractFactory("NAME")
-    const contract1 = await Contract1.deploy(VAR1, VAR2)
-    console.log("NAME deployed to:", contract1.target)
+    console.log("MyToken deployed to:", myToken.target)
 
-    // Set properties
-    console.log("Setting Properties...")
-    await contract1.writeFunctionName("test")
+    const stakingContract = await ethers.deployContract("Staking", [myToken.target])
+    await stakingContract.waitForDeployment()
+
+    console.log("staking contract deployed to:", stakingContract.target)
 }
 
 main().catch((error) => {
--- sc/scripts/interact.js
+++ sc/scripts/interact.js
@@ -8,18 +8,23 @@ async function main() {
 
     if (currentNetwork.chainId.toString().includes(1337)) {
         //we are on a local network
-        contract = await ethers.getContractAt("NAME", CONTRACT_ADDRESS_LOCAL)
+        contract = await ethers.getContractAt("Staking", CONTRACT_ADDRESS_LOCAL)
     } else {
         //we are on a remote network
-        contract = await ethers.getContractAt("NAME", CONTRACT_ADDRESS)
+        contract = await ethers.getContractAt("Staking", CONTRACT_ADDRESS)
     }
 
+    // we only specified 1 PK in hardhat.config.js => to execute this script on Sepolia, a second PK needs to be added
     const [signer1, signer2] = await ethers.getSigners()
 
-    console.log("Test Read: ", await contract.readFunctionName())
+    console.log("Minimum Stake Amount: ", ethers.formatEther(await contract.minimumStakeAmount()))
 
-    txn = await contract.writeFunctionName("test")
+    console.log("Annual Reward Rate: ", await contract.annualRewardRate())
+
+    txn = await contract.setMinimumStakeAmount(ethers.parseEther("5"))
     await txn.wait()
+
+    console.log("Minimum Stake Amount: ", ethers.formatEther(await contract.minimumStakeAmount()))
 }
 
 main()
