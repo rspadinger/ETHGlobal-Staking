7ed159c10e777b0a363e781c14dbdc873875dc16
Robert Spadinger <rspadinger@gmail.com>
2026-04-04 11:24:12 +0200

    add a simple spinner and logo

---
+++ ui/components/staking/loading-spinner.tsx
@@ -0,0 +1,7 @@
+export function LoadingSpinner() {
+    return (
+        <span className="inline-flex items-center justify-center">
+            <span className="animate-spin rounded-full h-4 w-4 border-b-2 border-primary"></span>
+        </span>
+    )
+}
